5b1c1b9a801f7bbf22dd938a1dbeed812c03c8ae
TanyaMaximova1 <tatamurom@gmail.com>
2023-02-19 23:16:17 +0300

    step2

--- index.js
+++ index.js
@@ -1,93 +1,3 @@
-// let i = 0;
-// for (let i = 2; i < 10; i++) {
-//     if (i % 2 == 0) {
-//         console.log(i);
-//     }
-// }
-
-// for (let i = 0; i < 3; i++) {
-//     console.log(i);
-// }
-
-
-// let i = 0;
-// while (++i < 5) alert(i);
-
-// и
-
-// let i = 0;
-// while (i++ < 5) alert(i);
-
-////////////////////////////////////////////////////
-// let task;
-// let listTasks;
-// function creatTasks() {
-//     task = document.getElementById('task').value;
-//     listTasks = document.getElementById('listOfTasks__list').value;
-//     for (let i = 0; i < 3; i++) {
-//         document.getElementById("listOfTasks__list").innerHTML = `<p>` + task + `</p>`;
-//     }
-//     creatTasks();
-// }
-////////////////////////////////////////////////////
-
-// let paragraph = document.getElementById("p");
-// function creatTasks() {
-//     for (let task = 0; task < 3; task++) {
-//         // document.getElementById("listOfTasks__list").innerHTML = `<p>` + task + `</p>`;
-//         let text = document.createTextNode(task);
-//         paragraph.appendChild(text);
-//     }
-
-// }
-
-// let task = document.querySelector('#task');
-
-// function creatTasks()
-// task = document.getElementById('task').value; {
-//     for (let i = 1; i <= 9; i++) {
-//         let p = document.createElement('p');
-//         p.innerHTML = i;
-//         task.appendChild(p);
-//     }
-//     creatTasks();
-// }
-
-// let parent = document.querySelector('#parent');
-
-// for (let i = 1; i <= 9; i++) {
-//     let li = document.createElement('li');
-//     li.innerHTML = i;
-//     parent.appendChild(li);
-// }
-
-// parg -- ссылка на элемент <p>
-
-// if (parg.hasChildNodes()) {
-//     // Таким образом, сначала мы проверяем, не пуст ли объект, есть ли у него дети
-//     var children = parg.childNodes;
-
-//     for (var i = 0; i < children.length; ++i) {
-//         // сделать что-то с каждым внутренним элементом через children[i]
-//         // ПРИМЕЧАНИЕ: Список является ссылкой, Добавление или удаление дочерних элементов изменит список
-//     }
-// }
-
-// let nodeList = document.body.childNodes;
-// let numb = document.getElementById("p").childNodes.length;
-// console.log(numb);
-
-
-
-// // Check
-// document.getElementById("checkbox").checked = true;
-
-// // Uncheck
-// document.getElementById("checkbox").checked = false;
-
-// let alertNoTasks = document.getElementById('noTasks__id');
-// if ()
-
 let task;
 let listTasks;
 task = document.getElementById('task').value;
@@ -134,37 +44,40 @@ function removeTasks() {
 
 
 //ЗАДАНИЕ ПОД ***
-let num1;
+let num1 = [];
 num1 = Number(num1);
 num1 = prompt("Введите число: ");
+let sum = 0;
 
-function getLength() {
-    for (let i = 1; i < num1.length + 1; i++) {
-        console.log(i);
+//метод выводит цифры, из которых состоит число
+function showNumbers() {
+    for (let numbers of num1) {
+        console.log(numbers);
     }
 }
-getLength();
+showNumbers();
 
-// function getLengthNumber() {
-//     for (let i = 0; i < num1.length; i++) {
-//         console.log(i.length);
-//     }
-// }
-// getLengthNumber();
-
+//Выводит сумму цифр числа
 function getSumNumber() {
     let sum = 0;
-    for (let i = 1; i < num1.length; i++) {
-        sum += num1[i];
-        console.log(sum);
+    for (let numbers of num1) {
+        sum += +numbers;
     }
+    console.log("Сумма цифр фисла: " + sum);
 }
 getSumNumber();
 
+//Выводит количество цифр числа
+function getLengthNumber() {
+    console.log("Количество цифр в числе: " + num1.length);
+}
+getLengthNumber();
 
-
-
-
-
-
-
+//Выводит цифры в обратном порядке
+function showNumbersBackToFront() {
+    var rez = num1[num1.length - 1];
+    for (var i = num1.length - 2; i >= 0; i--)
+        rez += num1[i];
+    console.log("Число перезаписано в обратном порядке: " + rez);
+}
+showNumbersBackToFront();
